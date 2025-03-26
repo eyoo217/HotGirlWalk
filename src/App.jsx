b2b4cdb6f81f6b420af78c2e77a4fff90d4a3fe5
Elliott Yoo <elliott.yoo17@gmail.com>
2025-03-26 17:58:09 -0500

    basename app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
 
   return (
-    <Router>
+    <Router basename="/HotGirlWalk/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/results" element={<Results />} />
